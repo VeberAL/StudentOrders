abfea4f868a3ef69a2f380f4990575c23964c9ed
VeberAL <aleo83@rambler.ru>
2021-12-08 17:44:48 +0300

    Сравнение скорости работы ArrayList и LinkedList

--- src/Collections/Comparison.java
+++ src/Collections/Comparison.java
@@ -13,6 +13,22 @@ public class Comparison {
             Alist.add(i);
             Llist.add(i);
         }
+        //ЧТЕНИЕ - ArrayList быстрее
+        long ArrayT0 = System.currentTimeMillis();
+        for(int i = 0; i < 100000; i++){
+            Alist.get(5000);
+        }
+        System.out.println("Время работы ArrayList: "+(System.currentTimeMillis()-ArrayT0));
+        //(вышло 1 мс)
+
+        long LinkedT0 = System.currentTimeMillis();
+        for(int i = 0; i < 100000; i++){
+            Llist.get(5000);
+        }
+        System.out.println("Время работы LinkedList: "+(System.currentTimeMillis()-LinkedT0));
+        //(вышло 740 мс)
+
+        //ЗАПИСЬ (в начало коллекции) - LinkedList быстрее
         //Добавляем 1000 раз элемент в индекс 0 и высчитываем время работы
         long ArrayT = System.currentTimeMillis();
         for(int i = 0; i < 1000; i++){
@@ -20,6 +36,7 @@ public class Comparison {
         }
         System.out.println("Время работы ArrayList: "+(System.currentTimeMillis()-ArrayT));
         //(вышло 15 мс)
+
         long LinkedT = System.currentTimeMillis();
         for(int i = 0; i < 1000; i++){
             Llist.add(0,i);
